acc81760e6f16ee0707c5bd917dc85b633c94b86
Malikil <eboy.coltt5@gmail.com>
2018-03-24 08:17:24 -0700

    Show default values as hints in add player

--- app/src/main/java/group10/glicko2calculator/AddPlayerActivity.java
+++ app/src/main/java/group10/glicko2calculator/AddPlayerActivity.java
@@ -1,13 +1,48 @@
 package group10.glicko2calculator;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class AddPlayerActivity extends AppCompatActivity {
-
+public class AddPlayerActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_player);
+
+        final EditText pName = findViewById(R.id.nameEntry),
+                    ratingEntry = findViewById(R.id.ratingEntry),
+                    deviationEntry = findViewById(R.id.deviationEntry),
+                    volatilityEntry = findViewById(R.id.volatilityEntry);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        float defaultRating = preferences.getFloat("Default Rating", 1500),
+                defaultDeviation = preferences.getFloat("Default Deviation", 350),
+                defaultVolatility = preferences.getFloat("Default Volatility", 0.06F);
+
+        ratingEntry.setHint(Float.toString(defaultRating));
+        deviationEntry.setHint(Float.toString(defaultDeviation));
+        volatilityEntry.setHint(Float.toString(defaultVolatility));
+
+        ((Button)findViewById(R.id.saveButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                // TODO Add new player to database
+            }
+        });
+        ((Button)findViewById(R.id.cancelButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                finish();
+            }
+        });
     }
 }
