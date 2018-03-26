8516a9a1dd6910afe2aa1caebda134816bb3d48c
Malikil <eboy.coltt5@gmail.com>
2018-03-26 00:54:03 -0700

    Added database to games activity

--- app/src/main/java/group10/glicko2calculator/GamesActivity.java
+++ app/src/main/java/group10/glicko2calculator/GamesActivity.java
@@ -1,15 +1,13 @@
 package group10.glicko2calculator;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
 
 public class GamesActivity extends AppCompatActivity {
 
@@ -19,7 +17,10 @@ public class GamesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_games);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        putInToolbar(toolbar);
+
+        ((ListView)findViewById(R.id.gamesList)).setAdapter(
+                new GamesAdapter(this, DatabaseHandler.getGames())
+        );
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -30,21 +31,4 @@ public class GamesActivity extends AppCompatActivity {
             }
         });
     }
-    private void putInToolbar(Toolbar toolbar) {
-        Drawable drawable = getResources().getDrawable(R.drawable.arrow);
-        drawable = resize(drawable, 64, 64);
-        toolbar.setNavigationIcon(drawable);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-    }
-    private Drawable resize(Drawable image, int width, int height) {
-        Bitmap b = ((BitmapDrawable)image).getBitmap();
-        Bitmap bitmapResized = Bitmap.createScaledBitmap(b, width, height, false);
-        return new BitmapDrawable(getResources(), bitmapResized);
-    }
-
 }
