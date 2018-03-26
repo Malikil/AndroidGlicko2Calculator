9dec46bf50d9662227f8b5a59eecbe2b18d67725
andreja6 <andrejaandjic@hotmail.com>
2018-03-25 18:22:08 -0700

    Added back button and resize function

--- app/src/main/java/group10/glicko2calculator/PlayersActivity.java
+++ app/src/main/java/group10/glicko2calculator/PlayersActivity.java
@@ -1,6 +1,9 @@
 package group10.glicko2calculator;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,13 +17,10 @@ public class PlayersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_players);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        // Load players from database into adapter
-        
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        putInToolbar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
@@ -28,6 +28,23 @@ public class PlayersActivity extends AppCompatActivity {
                 startActivity(new Intent(PlayersActivity.this, AddPlayerActivity.class));
             }
         });
+    }
+
+    private void putInToolbar(Toolbar toolbar) {
+        Drawable drawable = getResources().getDrawable(R.drawable.arrow);
+        drawable = resize(drawable, 64, 64);
+        toolbar.setNavigationIcon(drawable);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    private Drawable resize(Drawable image, int width, int height) {
+        Bitmap b = ((BitmapDrawable)image).getBitmap();
+        Bitmap bitmapResized = Bitmap.createScaledBitmap(b, width, height, false);
+        return new BitmapDrawable(getResources(), bitmapResized);
     }
 
 }
